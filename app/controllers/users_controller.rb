aa9935bc70ef73946ce1798dba3b360e56378d5c
yyyyy <ipooo346@gmail.com>
2020-11-11 13:23:15 -0300

    adding user controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,12 @@ class UsersController < ApplicationController
   end
 
   def create
+
+    @user = User.create(params.require(:user).permit(:username, :password))
+
+    sessions[:user_id] = @user.id
+
+    redirect_to '/welcome'
+  
   end
 end
